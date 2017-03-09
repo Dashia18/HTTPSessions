2dd67f6d5d5fcd6b24dbe923f472216544ba999e
Dashia <das-bobkova@yandex.ru>
2017-03-09 15:14:26 +0300

    Refactoring

--- src/myHttpSessionServlet/CreateUserServlet.java
+++ src/myHttpSessionServlet/CreateUserServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.*;
 
 
@@ -16,13 +17,6 @@ public class CreateUserServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        super.doGet(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         String user = (String) req.getParameter("login");
         String password = (String) req.getParameter("password");
@@ -44,8 +38,22 @@ public class CreateUserServlet extends HttpServlet{
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        HttpSession httpSession = req.getSession(true);
+        httpSession.setAttribute("user", user);
+        httpSession.setAttribute("password", password);
+
         resp.setStatus(HttpServletResponse.SC_OK);
         req.setAttribute("massage1","Congratulations, "+ user + ", you create profile!");
         req.getRequestDispatcher("/result.jsp").forward(req, resp);
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        super.doPost(req, resp);
+
     }
 }

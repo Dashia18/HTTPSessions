02971861b7c4218591f0cc453d9c6a7779e45589
Dashia <das-bobkova@yandex.ru>
2017-03-13 22:01:49 +0300

    Refactor main logic

--- src/myHttpSessionServlet/VerificationFormServlet.java
+++ src/myHttpSessionServlet/VerificationFormServlet.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 @WebServlet("/verificationFormServlet")
@@ -40,50 +39,55 @@ public class VerificationFormServlet extends HttpServlet{
         }
 
 
-        private static String[][] getDataAsArray(String path){
-            List<String> strList = fileScannerToSrtList(path);
-            return listToArrayLoginPwd(strList);
-        }
-        private static List<String> fileScannerToSrtList(String path) {
-            List<String> strList = new LinkedList<>();
-            try {
-                Scanner in = new Scanner(new File(path));
-                while (in.hasNext()){
-                    strList.add(in.nextLine());
-                }
-            } catch ( Exception ex ) {
-                ex.printStackTrace();
+    private static String[][] getDataAsArray(String path){
+        List<String> strList = fileScannerToSrtList(path);
+        return listToArrayLoginPwd(strList);
+    }
+    private static List<String> fileScannerToSrtList(String path) {
+        List<String> strList = new LinkedList<>();
+        try {
+            Scanner in = new Scanner(new File(path));
+            while (in.hasNext()){
+                strList.add(in.nextLine());
             }
-            return strList;
+        } catch ( Exception ex ) {
+            ex.printStackTrace();
         }
-        private static String[][] listToArrayLoginPwd(List<String> strList){
-            String[][] arr = new String[strList.size()][2];
-            Pattern pattern2 = Pattern.compile("[ ,!?\\[\\]]");
-            int i = 0;
-            for (String str:strList){
-                String[] words = pattern2.split(str);
-                arr[i][0] = words[0];//0 col
-                arr[i][1] = words[1];//1 col
-                i++;
-            }
-            return arr;
+        return strList;
+    }
+    private static String[][] listToArrayLoginPwd(List<String> strList){
+        String[][] arr = new String[strList.size()][2];
+        Pattern pattern2 = Pattern.compile("[ ,!?\\[\\]]");
+        int i = 0;
+        for (String str:strList){
+            String[] words = pattern2.split(str);
+            arr[i][0] = words[0];//0 col
+            arr[i][1] = words[1];//1 col
+            i++;
         }
+        return arr;
+    }
 
-        private static void verifyUser(String[][] arr,
+    private static void verifyUser(String[][] arr,
                                        HttpServletRequest req, HttpServletResponse resp) {
 
-            String user = (String) req.getParameter("login");
-            String password = (String) req.getParameter("password");
-            String remember= (String)req.getParameter("ischecked");
+        String user = (String) req.getParameter("login");
+        String password = (String) req.getParameter("password");
+        String remember= (String)req.getParameter("ischecked");
+
             System.out.println("remember = " + remember);
 
-            HttpSession httpSession= req.getSession(true);
+        HttpSession httpSession= req.getSession(true);
             System.out.println("httpSession.isNew() = " + httpSession.isNew());
 
             System.out.println("httpSession.getAttribute(\"user in\") "+httpSession.getAttribute("user"));
             System.out.println("httpSession.getAttribute(\"password in\") " + httpSession.getAttribute("password"));
+            System.out.println("httpSession.getAttribute(\"pwdTry in\") "+httpSession.getAttribute("pwdTry"));
 
-            if (null == httpSession.getAttribute("user") &&  null == httpSession.getAttribute("password")){
+        Integer pwdTry = getPwdTry(req, resp, user, httpSession);
+
+
+            if (httpSession.getAttribute("user") == null ||  httpSession.getAttribute("password")== null ){
 
                 System.out.println("NEW SESSION");
                 System.out.println(httpSession.getId());
@@ -108,64 +112,17 @@ public class VerificationFormServlet extends HttpServlet{
                 }
                 if (loginOk) {
                     if (pwdOk) {
-                        //login & psw ok
-                        resp.setStatus(HttpServletResponse.SC_OK);
-                        req.setAttribute("massage1", "Congratulations, " + user + ", you login!");
-                        try {
-                            req.getRequestDispatcher("/result.jsp").forward(req, resp);
-                        } catch (ServletException | IOException e) {
-                            e.printStackTrace();
-                        }
-
-                        if("on".equals(remember)) {
-
-                            httpSession.setAttribute("user", user);
-                            httpSession.setAttribute("password", password);
-                            System.out.println("httpSession.getAttribute(\"user out\") "+httpSession.getAttribute("user"));
-                            System.out.println("httpSession.getAttribute(\"password out\") " + httpSession.getAttribute("password"));
-                        }
-
-
+                        loginUser(req, resp, user, password, remember, httpSession, pwdTry);
                     } else {
-                        //login ok & pwd bad
-                        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                        req.setAttribute("massage1", user + ", password is not correct! ");
-                        try {
-                            req.getRequestDispatcher("/result.jsp").forward(req, resp);
-                        } catch (ServletException | IOException e) {
-                            e.printStackTrace();
-                        }
+                        badPwdLogin(req, resp, user, remember, httpSession, pwdTry);
                     }
                 }
                 else {
-                    //new user
-                    resp.setStatus(HttpServletResponse.SC_OK);
-                    System.out.println("GO TO CREATE file");
-
-                    try {
-                        req.getRequestDispatcher("/create.html").forward(req, resp);
-                    } catch (ServletException | IOException e) {
-                        e.printStackTrace();
-                    }
-                    return;
+                    createUser(req, resp);
                 }
             }
             else {
-                System.out.println("OLD SESSION");
-                System.out.println(httpSession.getId());
-                System.out.println(httpSession.getAttribute("user"));
-                System.out.println(httpSession.getAttribute("password"));
-
-                String userSessionName = (String) httpSession.getAttribute("user");
-
-                resp.setStatus(HttpServletResponse.SC_OK);
-                req.setAttribute("massage1", "Congratulations, " + userSessionName + ", you login!");
-                try {
-                    req.getRequestDispatcher("/result.jsp").forward(req, resp);
-                } catch (ServletException | IOException e) {
-                    e.printStackTrace();
-                }
-
+                openOldSession(req, resp, httpSession);
             }
 
             if(null == remember) {
@@ -173,7 +130,106 @@ public class VerificationFormServlet extends HttpServlet{
                 System.out.println("clear httpSession");
             }
 
-
         }
+    
+    private static Integer getPwdTry(HttpServletRequest req, HttpServletResponse resp,
+                                     String user, HttpSession httpSession) {
+
+        Integer pwdTry = (Integer)httpSession.getAttribute("pwdTry");
+        if(pwdTry == null || !user.equals(httpSession.getAttribute("user"))){
+            pwdTry = 0;
+        }
+        else {
+            if(pwdTry>2){
+                resp.setStatus(HttpServletResponse.SC_OK);
+                System.out.println("GO TO UPDATE password");
+
+                try {
+                    req.getRequestDispatcher("/pwdchange.html").forward(req, resp);
+                } catch (ServletException | IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("pwdTry = " + pwdTry);
+        return pwdTry;
+    }
+
+    private static void loginUser(HttpServletRequest req, HttpServletResponse resp, String user,
+                                  String password, String remember, HttpSession httpSession, Integer pwdTry) {
+        //login & psw ok
+        resp.setStatus(HttpServletResponse.SC_OK);
+        req.setAttribute("massage1", "Congratulations, " + user + ", you login!");
+        try {
+            req.getRequestDispatcher("/result.jsp").forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+
+        if("on".equals(remember)) {
+
+            httpSession.setAttribute("user", user);
+            httpSession.setAttribute("password", password);
+            httpSession.setAttribute("pwdTry", pwdTry);
+
+            System.out.println("httpSession.getAttribute(\"user out\") "+httpSession.getAttribute("user"));
+            System.out.println("httpSession.getAttribute(\"password out\") " + httpSession.getAttribute("password"));
+            System.out.println("httpSession.getAttribute(\"pwdTry out\") "+httpSession.getAttribute("pwdTry"));
+        }
+    }
+
+    private static void badPwdLogin(HttpServletRequest req, HttpServletResponse resp, String user,
+                                    String remember, HttpSession httpSession, Integer pwdTry) {
+        //login ok & pwd bad
+        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        if("on".equals(remember)) {
+            pwdTry++;
+            System.out.println("pwdTry = " + pwdTry);
+            httpSession.setAttribute("user", user);
+            httpSession.setAttribute("password", null);
+            httpSession.setAttribute("pwdTry", pwdTry);
+
+            System.out.println("httpSession.getAttribute(\"user out\") "+httpSession.getAttribute("user"));
+            System.out.println("httpSession.getAttribute(\"password out\") " + httpSession.getAttribute("password"));
+            System.out.println("httpSession.getAttribute(\"pwdTry out\") "+httpSession.getAttribute("pwdTry"));
+        }
+
+        req.setAttribute("massage1", user + ", password is not correct! ");
+
+        try {
+            req.getRequestDispatcher("/result.jsp").forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createUser(HttpServletRequest req, HttpServletResponse resp) {
+        //new user
+        resp.setStatus(HttpServletResponse.SC_OK);
+        System.out.println("GO TO CREATE file");
+
+        try {
+            req.getRequestDispatcher("/create.html").forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void openOldSession(HttpServletRequest req, HttpServletResponse resp, HttpSession httpSession) {
+        System.out.println("OLD SESSION");
+        System.out.println(httpSession.getId());
+        System.out.println(httpSession.getAttribute("user"));
+        System.out.println(httpSession.getAttribute("password"));
+
+        String userSessionName = (String) httpSession.getAttribute("user");
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+        req.setAttribute("massage1", "Congratulations, " + userSessionName + ", you login!");
+        try {
+            req.getRequestDispatcher("/result.jsp").forward(req, resp);
+        } catch (ServletException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
